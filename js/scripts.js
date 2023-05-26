b1dc1196fc299c15d85fc44b97dd6d88e36cac93
Medina <110483791+medina003@users.noreply.github.com>
2023-05-26 20:01:29 +0400

    section work

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-$(".owl-carousel").owlCarousel({
+$("#carusel1").owlCarousel({
     margin: 40,
     nav: true,
     dots: false,
     items: 4,
     navText: ['', ''],
+    autoWidth: true,
     responsive: {
         0: {
             items: 1,
@@ -16,4 +17,14 @@ $(".owl-carousel").owlCarousel({
             items: 5,
         },
     },
+});
+
+
+$("#carusel2").owlCarousel({
+    margin: 40,
+    nav: true,
+    dots: false,
+    items: 3,
+    navText: ['', ''],
+    autoWidth: true,
 });
